cdc99b3891b8814b73b45e52d6c7b33b458b625b
Clara <clarapachecodev@gmail.com>
2022-12-07 08:50:44 -0300

    Declare  class 'Despesa' and a new instance for the object cadastrarDespesa1 + force empty value for 'Tipo' in select option

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+class Despesa{
+  constructor(ano,mes,dia,tipo,descricao,valor){
+    this.ano = ano;
+    this.mes = mes;
+    this.dia = dia;
+    this.tipo = tipo;
+    this.descricao = descricao;
+    this.valor = valor
+  }
+}
+
 let button = document.querySelector('#button')
 
 button.addEventListener('click', function cadastrarDespesa(){
@@ -9,13 +20,16 @@ button.addEventListener('click', function cadastrarDespesa(){
   let descricao = document.querySelector('#descricao')
   let valor = document.querySelector('#valor')
 
-  console.log(ano.value)
-  console.log(mes.value)
-  console.log(dia.value)
-  console.log(tipo.value)
-  console.log(descricao.value)
-  console.log(valor.value)
-
+  let cadastroDespesa1 = new Despesa(
+    ano.value,
+    mes.value,
+    dia.value,
+    tipo.value,
+    descricao.value,
+    valor.value
+  )
+  
+  console.log(cadastroDespesa1)
 
 })
 
