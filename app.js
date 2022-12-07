8d7930fa78166e2a3294f2a7ac022d3980df37a6
Clara <clarapachecodev@gmail.com>
2022-12-07 07:56:53 -0300

    Create function that is called when a click event on the button happens;get elements from form via DOM

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-alert('OK!')
+let button = document.querySelector('#button')
+
+button.addEventListener('click', function cadastrarDespesa(){
+
+  let ano = document.querySelector('#ano')
+  let mes = document.querySelector('#mes')
+  let dia = document.querySelector('#dia')
+  let tipo = document.querySelector('#tipo')
+  let descricao = document.querySelector('#descricao')
+  let valor = document.querySelector('#valor')
+
+  console.log(ano.value)
+  console.log(mes.value)
+  console.log(dia.value)
+  console.log(tipo.value)
+  console.log(descricao.value)
+  console.log(valor.value)
+
+
+})
+
